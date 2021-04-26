694acb3762c9d587033b039bce783b9ac3e8d7d4
Reece Tarrant <rtarrant70998@gmail.com>
2021-04-26 22:59:09 +0100

    Making a start on the FSM

---
+++ COMP2007 Main Assignment/Assets/Scripts/FSM/Manager/StateManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// First attempt at implementing my own Finite State Machine (in any and all works linked to this
+/// assignment, we will be using the FSM abbrieviation when refering to this).
+///</summary>
+
+public class StateManager : MonoBehaviour
+{
+    private IBaseState IActiveState;
+
+    public static StateManager InstanceRef = null;
+    private static StateManager instanceRef;
+
+    private void Awake() 
+    {
+        if(instanceRef != null)
+        {
+            
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
